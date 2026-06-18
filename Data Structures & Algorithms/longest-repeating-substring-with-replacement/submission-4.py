fed01e036873f252c07830ca315b5230c51fff2c
Fast-Furious2023 <126244912+Fast-Furious2023@users.noreply.github.com>
2026-06-18 16:06:05 -0700

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        #return the length of the longest substring of one repeating character
+        count = {}
+        l = 0
+        longest = 0
+        maxq = 0
+        for r in range(len(s)):
+            count[s[r]] = count.get(s[r],0) + 1
+            maxq = max(maxq, count[s[r]])
+            while (r-l+1) - maxq > k:
+                
+                count[s[l]]-=1
+                l += 1
+            longest = max(longest, r-l+1)
+        return longest
