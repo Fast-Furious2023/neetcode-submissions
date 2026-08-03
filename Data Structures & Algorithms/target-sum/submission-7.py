d1b164737c47eb28e269b297d1704d88929a6063
Fast-Furious2023 <126244912+Fast-Furious2023@users.noreply.github.com>
2026-08-03 06:22:49 -0700

    Add: target-sum - submission-7

---
+++ Data Structures & Algorithms/target-sum/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
+
+        def helper(i,remain):
+            if i == len(nums):
+                return 1 if remain == 0 else 0
+
+            if (i,remain) in memo:
+                return memo[(i,remain)]
+        # add
+            add = helper(i+1,remain-nums[i])
+        # minus
+            minus = helper(i+1,remain+nums[i])
+
+            memo[(i,remain)]=add+minus
+
+            return memo[(i,remain)]
+
+        return helper(0,target)
+
