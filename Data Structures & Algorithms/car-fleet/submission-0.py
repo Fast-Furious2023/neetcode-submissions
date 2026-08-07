ab559ec547e8c6eec99f48cf23ddc529a315f4a7
Fast-Furious2023 <126244912+Fast-Furious2023@users.noreply.github.com>
2026-08-07 03:58:35 -0700

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        #use a hashtable for O(1) lookup
+        table = {}
+        n = len(position)
+        res=0
+        ahead = 0 #the time the car ahead of it reaches destination
+
+        for i in range(n):
+            table[position[i]]=speed[i]
+
+        p_sorted = sorted(position)
+
+        for i in range(n-1,-1,-1):
+            pos = p_sorted[i]
+            spd = table[pos]
+            hour = (target-pos)/spd
+            if hour > ahead:
+                res+=1
+                ahead = hour
+            elif hour <= ahead:
+                continue
+        return res
+
+
+
+
