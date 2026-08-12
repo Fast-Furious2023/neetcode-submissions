ccdf5bf18c02cc83b3ca8f3322b06bf9cbd8aaa7
Fast-Furious2023 <126244912+Fast-Furious2023@users.noreply.github.com>
2026-08-12 15:29:00 -0700

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.py
@@ -0,0 +1,25 @@
+from functools import cache
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        #sum is divisble by 2--> possible; if not, false
+
+        total = sum(nums)
+        n = len(nums)
+        if total%2 == 1:
+            return False
+
+        target= total//2
+        
+        curSum = 0
+
+        @cache
+        def helper(idx, c_sum):
+            if c_sum == target:
+                return True
+            if idx >= n or c_sum > target:
+                return False
+        
+
+            return helper(idx+1, c_sum + nums[idx]) or helper(idx+1, c_sum)
+
+        return helper(0, 0)
